0a184f85ae1a1d80ca55a3e8c91b2d0f70f6a429
deacix <info@deacix.de>
2019-06-22 22:08:07 +0300

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+# Approvede.zone
+
+Deploy smart contract. In a new console tab run:
+
+```
+    etherlime deploy
+```
+
+Start dApp on localhost.
+```
+    cd web
+    ng serve --open
+```
---
+++ deployment/deploy.js
@@ -0,0 +1,13 @@
+// const etherlime = require('etherlime');
+// const ToDo = require('../build/ToDoManager.json');
+//
+// const deploy = async (network, secret) => {
+//
+// 	const deployer = new etherlime.EtherlimeGanacheDeployer();
+// 	const contract = await deployer.deploy(ToDo);
+//
+// };
+//
+// module.exports = {
+// 	deploy
+// };
